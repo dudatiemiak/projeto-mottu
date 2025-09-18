312e6cdc3dc4ee1e70a52cd8cb38e60e527fc2f1
dudatiemiak <rm554756@fiap.com.br>
2025-09-18 00:01:33 -0300

    Arrumando controller de cliente, para inserir, editar, excluir e mostrar detalhes de cliente, tentando incluir tabela de telefone. E adicionando as páginas html e rotas para essas finalidades

--- src/main/java/br/com/fiap/projeto_mottu/control/ClienteController.java
+++ src/main/java/br/com/fiap/projeto_mottu/control/ClienteController.java
@@ -11,32 +11,28 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.fiap.projeto_mottu.dto.ClienteDTO;
-import br.com.fiap.projeto_mottu.model.Bairro;
-import br.com.fiap.projeto_mottu.model.Cidade;
 import br.com.fiap.projeto_mottu.model.Cliente;
-import br.com.fiap.projeto_mottu.model.Estado;
 import br.com.fiap.projeto_mottu.model.Funcionario;
-import br.com.fiap.projeto_mottu.model.Logradouro;
-import br.com.fiap.projeto_mottu.model.Pais;
+import br.com.fiap.projeto_mottu.model.Telefone;
 import br.com.fiap.projeto_mottu.repository.ClienteRepository;
 import br.com.fiap.projeto_mottu.repository.FuncionarioRepository;
 import br.com.fiap.projeto_mottu.repository.LogradouroRepository;
 import br.com.fiap.projeto_mottu.repository.TelefoneRepository;
 import br.com.fiap.projeto_mottu.service.ClienteCachingService;
 import br.com.fiap.projeto_mottu.service.ClienteService;
+import jakarta.validation.Valid;
 
 @Controller
 public class ClienteController {
@@ -150,42 +146,149 @@ public class ClienteController {
 	}
 	
 	@GetMapping("/cliente/lista")
-	public ModelAndView popularIndex() {
-
-		ModelAndView mv = new ModelAndView("/home/index");
-		
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    public ModelAndView listarClientes() {
+        ModelAndView mv = new ModelAndView("/cliente/lista"); 
+        
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
 		Optional<Funcionario> op = repFunc.findByUsername(auth.getName());
 		
 		if(op.isPresent()) {
 			mv.addObject("funcionario", op.get());
 		}
-		
-		mv.addObject("clientes", repC.findAll());
+        
+        mv.addObject("clientes", repC.findAll());
+        return mv;
+    }
 
-		return mv;
-	}
-	
-	@GetMapping("/logradouro/nova")
-	public ModelAndView retornaPagCadPessoa() {
-		
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		
+    @GetMapping("/cliente/nova")
+    public ModelAndView formularioNovoCliente() {        
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        
 		Optional<Funcionario> op = repFunc.findByUsername(auth.getName());
 
-		ModelAndView mv = new ModelAndView("/logradouro/nova");
-
-		if(op.isPresent()) {
+        ModelAndView mv = new ModelAndView("/cliente/nova");
+        
+        if(op.isPresent()) {
 			mv.addObject("funcionario",op.get());
 		}
-		
-		mv.addObject("logradouro", new Logradouro());
-		mv.addObject("pais", new Pais());
-		mv.addObject("estado", new Estado());
-		mv.addObject("cidade", new Cidade());
-		mv.addObject("bairro", new Bairro());
 
-		return mv;
-	}
+        mv.addObject("cliente", new Cliente());
+        mv.addObject("logradouros", repL.findAll());
+        mv.addObject("telefones", repT.findAll());
+        return mv;
+    }
+
+    @PostMapping("/cliente/inserir")
+    public ModelAndView inserirCliente(@Valid Cliente cliente, BindingResult bd, @Valid Telefone telefone) {
+        if (bd.hasErrors()) {
+            ModelAndView mv = new ModelAndView("/cliente/nova");
+            
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            
+    		Optional<Funcionario> op = repFunc.findByUsername(auth.getName());
+    		
+    		if(op.isPresent()) {
+				mv.addObject("funcionario", op.get());
+			}
+    		
+            mv.addObject("cliente", cliente);
+            mv.addObject("logradouros", repL.findAll());
+            mv.addObject("telefones", repT.findAll());
+            return mv;
+        }
+
+        repC.save(cliente);
+        telefone.setCliente(cliente);
+        repT.save(telefone);
+
+        return new ModelAndView("redirect:/cliente/lista");
+    }
+
+    @GetMapping("/cliente/detalhes/{id}")
+    public ModelAndView detalhesCliente(@PathVariable Long id) {
+        Optional<Cliente> op = repC.findById(id);
+        if (op.isPresent()) {
+            ModelAndView mv = new ModelAndView("/cliente/detalhes");
+            
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            
+    		Optional<Funcionario> op1 = repFunc.findByUsername(auth.getName());
+    		
+    		if(op1.isPresent()) {
+				mv.addObject("funcionario", op1.get());
+			}
+    		
+            mv.addObject("cliente", op.get());
+            return mv;
+        }
+        return new ModelAndView("redirect:/cliente/lista");
+    }
+
+    @GetMapping("/cliente/editar/{id}")
+    public ModelAndView editarClienteForm(@PathVariable Long id) {
+        Optional<Cliente> op = repC.findById(id);
+        if (op.isPresent()) {
+            ModelAndView mv = new ModelAndView("/cliente/editar");
+            
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            
+    		Optional<Funcionario> op1 = repFunc.findByUsername(auth.getName());
+    		
+    		if(op1.isPresent()) {
+				mv.addObject("funcionario", op1.get());
+			}
+            
+            mv.addObject("cliente", op.get());
+            mv.addObject("logradouros", repL.findAll());
+            mv.addObject("telefones", repT.findAll());
+            return mv;
+        }
+        return new ModelAndView("redirect:/cliente/lista");
+    }
+
+    @PostMapping("/cliente/editar/{id}")
+    public ModelAndView editarCliente(@PathVariable Long id, @Valid Cliente cliente, BindingResult bd, @Valid Telefone telefone) {
+        if (bd.hasErrors()) {
+            ModelAndView mv = new ModelAndView("/cliente/editar");
+            mv.addObject("cliente", cliente);
+            mv.addObject("logradouros", repL.findAll());
+            mv.addObject("telefones", repT.findAll());
+            return mv;
+        }
+
+        Optional<Cliente> op = repC.findById(id);
+        if (op.isPresent()) {
+            Cliente atual = op.get();
+            atual.setNm_cliente(cliente.getNm_cliente());
+            atual.setNr_cpf(cliente.getNr_cpf());
+            atual.setNm_email(cliente.getNm_email());
+            atual.setLogradouro(cliente.getLogradouro());
+            repC.save(atual);
+
+            Optional<Telefone> telOp = repT.findByCliente(atual); // precisa criar este método no TelefoneRepository
+            if (telOp.isPresent()) {
+                Telefone telAtual = telOp.get();
+                telAtual.setNr_ddd(telefone.getNr_ddd());
+                telAtual.setNr_ddi(telefone.getNr_ddi());
+                telAtual.setNr_telefone(telefone.getNr_telefone());
+                repT.save(telAtual);
+            } else {
+                telefone.setCliente(atual);
+                repT.save(telefone);
+            }
+        }
+        return new ModelAndView("redirect:/cliente/lista");
+    }
+
+    @GetMapping("/cliente/remover/{id}")
+    public ModelAndView removerClienteExistente(@PathVariable Long id) {
+    	Optional<Cliente> op = repC.findById(id);
+    	if(op.isPresent()) {
+    		repC.deleteById(id);
+            return new ModelAndView("redirect:/cliente/lista");
+    	}
+        return new ModelAndView("redirect:/cliente/lista");
+    }
+
 }
